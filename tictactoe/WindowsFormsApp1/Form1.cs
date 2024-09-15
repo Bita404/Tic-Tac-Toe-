52af3fbd71a5f5165996e4b6d27e12ad7903f901
Bitahua <87811454+Bita404@users.noreply.github.com>
2024-09-15 17:20:38 +0330

    Add files via upload

---
+++ tictactoe/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        char Player = 'X';
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button playbutton = sender as Button;
+            playbutton.Enabled = false;
+            playbutton.BackColor = Color.Cyan;
+            if (Player == 'X')
+            {
+                playbutton.Text = "X";
+                if ((A1.Text == A2.Text && A2.Text == A3.Text && A1.Text != "") ||
+                   (B1.Text == B2.Text && B2.Text == B3.Text && B1.Text != "") ||
+                   (C1.Text == C2.Text && C2.Text == C3.Text && C1.Text != "") ||
+                   (A1.Text == B1.Text && B1.Text == C1.Text && A1.Text != "") ||
+                   (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != "") ||
+                   (A3.Text == B3.Text && B3.Text == C3.Text && A3.Text != "") ||
+                   (A1.Text == B2.Text && B2.Text == C3.Text && A1.Text != "") ||
+                   (A3.Text == B2.Text && B2.Text == C1.Text && A3.Text != ""))
+                {
+                    MessageBox.Show(($" {Player.ToString()}  wins !!! :D"));
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B3.Enabled = false;
+                    B2.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+
+
+                }
+                else if (A1.Text!= "" && A2.Text != "" && A3.Text != "" &&
+                    B1.Text !="" && B2.Text !="" && B3.Text !="" &&
+                    C1.Text != "" && C2.Text != "" && C3.Text != "")
+                {
+                    MessageBox.Show("Game Over!!!");
+                }
+                Player = 'O';
+            }
+            else if (Player == 'O')
+            {
+                playbutton.Text = "O";
+                if ((A1.Text == A2.Text && A2.Text == A3.Text && A1.Text != "") ||
+                  (B1.Text == B2.Text && B2.Text == B3.Text && B1.Text != "") ||
+                  (C1.Text == C2.Text && C2.Text == C3.Text && C1.Text != "") ||
+                  (A1.Text == B1.Text && B1.Text == C1.Text && A1.Text != "") ||
+                  (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != "") ||
+                  (A3.Text == B3.Text && B3.Text == C3.Text && A3.Text != "") ||
+                  (A1.Text == B2.Text && B2.Text == C3.Text && A1.Text != "") ||
+                  (A3.Text == B2.Text && B2.Text == C1.Text && A3.Text != ""))
+                {
+                    MessageBox.Show(($" {Player.ToString()}  wins !!! :D"));
+                    A1.Enabled = false;
+                    A2.Enabled = false;
+                    A3.Enabled = false;
+                    B1.Enabled = false;
+                    B3.Enabled = false;
+                    B2.Enabled = false;
+                    C1.Enabled = false;
+                    C2.Enabled = false;
+                    C3.Enabled = false;
+
+                }
+                else if (A1.Text != "" && A2.Text != "" && A3.Text != "" &&
+                    B1.Text != "" && B2.Text != "" && B3.Text != "" &&
+                    C1.Text != "" && C2.Text != "" && C3.Text != "")
+                {
+                    MessageBox.Show("Game Over!!!");
+                }
+                    Player = 'X';
+            }
+        }
+
+        private void button_NewGame_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_NewGame_MouseEnter(object sender, EventArgs e)
+        {
+            button_NewGame.ForeColor = Color.Blue;
+        }
+
+        private void button_NewGame_MouseLeave(object sender, EventArgs e)
+        {
+            button_NewGame.ForeColor = Color.DarkBlue;
+        }
+
+        private void button_Exit_MouseEnter(object sender, EventArgs e)
+        {
+            button_Exit.ForeColor = Color.Blue;
+        }
+
+        private void button_Exit_MouseLeave(object sender, EventArgs e)
+        {
+            button_Exit.ForeColor = Color.DarkBlue;
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Done by Bita Saeed s9919314 - thanks for playing :D");
+        }
+
+        private void buttonInfo_MouseEnter(object sender, EventArgs e)
+        {
+            buttonInfo.ForeColor = Color.DeepPink;
+        }
+
+        private void buttonInfo_MouseLeave(object sender, EventArgs e)
+        {
+            buttonInfo.ForeColor = Color.Purple;
+        }
+    }
+}
---
+++ tictactoe/WindowsFormsApp1/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.button_NewGame = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.label_tic = new System.Windows.Forms.Label();
+            this.label_tac = new System.Windows.Forms.Label();
+            this.label_toe = new System.Windows.Forms.Label();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.A1.FlatAppearance.BorderSize = 4;
+            this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.A1.Location = new System.Drawing.Point(9, 12);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(139, 139);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.A2.FlatAppearance.BorderSize = 5;
+            this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.A2.Location = new System.Drawing.Point(145, 12);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(139, 139);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.A3.FlatAppearance.BorderSize = 5;
+            this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.A3.Location = new System.Drawing.Point(281, 12);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(139, 139);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.B1.FlatAppearance.BorderSize = 5;
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.B1.Location = new System.Drawing.Point(9, 148);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(139, 139);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.B2.FlatAppearance.BorderSize = 5;
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.B2.Location = new System.Drawing.Point(145, 148);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(139, 139);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.B3.FlatAppearance.BorderSize = 5;
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.B3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.B3.Location = new System.Drawing.Point(281, 148);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(139, 139);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.C1.FlatAppearance.BorderSize = 5;
+            this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.C1.Location = new System.Drawing.Point(9, 283);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(139, 139);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.C2.FlatAppearance.BorderSize = 5;
+            this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.C2.Location = new System.Drawing.Point(145, 283);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(139, 139);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(203)))), ((int)(((byte)(255)))));
+            this.C3.FlatAppearance.BorderSize = 5;
+            this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.C3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 80.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.C3.Location = new System.Drawing.Point(281, 283);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(139, 139);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button_NewGame
+            // 
+            this.button_NewGame.BackColor = System.Drawing.Color.Aqua;
+            this.button_NewGame.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_NewGame.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button_NewGame.Location = new System.Drawing.Point(432, 233);
+            this.button_NewGame.Name = "button_NewGame";
+            this.button_NewGame.Size = new System.Drawing.Size(165, 70);
+            this.button_NewGame.TabIndex = 9;
+            this.button_NewGame.Text = "New Game";
+            this.button_NewGame.UseVisualStyleBackColor = false;
+            this.button_NewGame.Click += new System.EventHandler(this.button_NewGame_Click);
+            this.button_NewGame.MouseEnter += new System.EventHandler(this.button_NewGame_MouseEnter);
+            this.button_NewGame.MouseLeave += new System.EventHandler(this.button_NewGame_MouseLeave);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.BackColor = System.Drawing.Color.Aqua;
+            this.button_Exit.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button_Exit.Location = new System.Drawing.Point(432, 309);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(165, 68);
+            this.button_Exit.TabIndex = 10;
+            this.button_Exit.Text = "Exit";
+            this.button_Exit.UseVisualStyleBackColor = false;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            this.button_Exit.MouseEnter += new System.EventHandler(this.button_Exit_MouseEnter);
+            this.button_Exit.MouseLeave += new System.EventHandler(this.button_Exit_MouseLeave);
+            // 
+            // label_tic
+            // 
+            this.label_tic.AutoSize = true;
+            this.label_tic.Font = new System.Drawing.Font("Ravie", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_tic.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label_tic.Location = new System.Drawing.Point(426, 9);
+            this.label_tic.Name = "label_tic";
+            this.label_tic.Size = new System.Drawing.Size(171, 86);
+            this.label_tic.TabIndex = 11;
+            this.label_tic.Text = "Tic";
+            // 
+            // label_tac
+            // 
+            this.label_tac.AutoSize = true;
+            this.label_tac.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label_tac.Font = new System.Drawing.Font("Ravie", 45F);
+            this.label_tac.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label_tac.Location = new System.Drawing.Point(426, 82);
+            this.label_tac.Name = "label_tac";
+            this.label_tac.Size = new System.Drawing.Size(182, 81);
+            this.label_tac.TabIndex = 12;
+            this.label_tac.Text = "Tac";
+            // 
+            // label_toe
+            // 
+            this.label_toe.AutoSize = true;
+            this.label_toe.Font = new System.Drawing.Font("Ravie", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_toe.ForeColor = System.Drawing.Color.Blue;
+            this.label_toe.Location = new System.Drawing.Point(424, 148);
+            this.label_toe.Name = "label_toe";
+            this.label_toe.Size = new System.Drawing.Size(184, 86);
+            this.label_toe.TabIndex = 13;
+            this.label_toe.Text = "Toe";
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonInfo.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.buttonInfo.Location = new System.Drawing.Point(432, 384);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(165, 38);
+            this.buttonInfo.TabIndex = 14;
+            this.buttonInfo.Text = "info";
+            this.buttonInfo.UseVisualStyleBackColor = false;
+            this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
+            this.buttonInfo.MouseEnter += new System.EventHandler(this.buttonInfo_MouseEnter);
+            this.buttonInfo.MouseLeave += new System.EventHandler(this.buttonInfo_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(603, 433);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.label_toe);
+            this.Controls.Add(this.label_tac);
+            this.Controls.Add(this.label_tic);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_NewGame);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe by s9919314";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button button_NewGame;
+        private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.Label label_tic;
+        private System.Windows.Forms.Label label_tac;
+        private System.Windows.Forms.Label label_toe;
+        private System.Windows.Forms.Button buttonInfo;
+    }
+}
+
